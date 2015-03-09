cd54a81f9afc6e020267a523d6eafe84d3bd704d
ccc <cadappl@gmail.com>
2015-03-09 22:11:44 +0800

    git_cmd: add to support git commands
    It's the encapsulation for git commands, which make the usage of
    git commands easily.
    
    Change-Id: I509c59436fceee4aaef738932b5e57d84f3ae34a

---
+++ topics/git_cmd.py
@@ -0,0 +1,99 @@
+
+from command import Command
+from file_utils import FileUtils
+
+
+class GitCommand(Command):
+    """Executes a git sub-command with specified parameters"""
+    def __init__(self, gitdir=None, worktree=None, *args, **kws):
+        Command.__init__(self, cwd=worktree, *args, **kws)
+
+        self.gitdir = gitdir
+        self.worktree = worktree
+        self.git = FileUtils.find_execute('git')
+
+    def _execute(self, *args, **kws):
+        cli = list()
+        cli.append(self.git)
+
+        gitdir = self.gitdir if self.gitdir else None
+        if self.worktree:
+            cli.append('--work-tree=%s' % self.worktree)
+            if not gitdir:
+                gitdir = FileUtils.ensure_path(self.worktree, '.git')
+
+        if gitdir:
+            cli.append('--git-dir=%s' % gitdir)
+
+        if len(args):
+            cli.extend(args)
+
+        self.new_args(cli)
+        return self.wait(**kws)
+
+    def raw_command(self, *args, **kws):
+        return self._execute(*args, **kws)
+
+    def raw_command_with_output(self, *args, **kws):
+        res = self._execute(capture_stdout=True, *args, **kws)
+
+        return res, self.get_output()
+
+    def set_path(self, gitdir=None, worktree=None):
+        if gitdir:
+            self.gitdir = gitdir
+
+        if worktree:
+            self.worktree = worktree
+
+    def init(self, *args, **kws):
+        return self.raw_command('init', *args, **kws)
+
+    def add(self, *args, **kws):
+        if len(args) == 0:
+            args = args[:]
+            args.append('.')
+
+        return self.raw_command('add', *args, **kws)
+
+    def branch(self, *args, **kws):
+        return self.raw_command_with_output('branch', *args, **kws)
+
+    def checkout(self, *args, **kws):
+        return self.raw_command('checkout', *args, **kws)
+
+    def clone(self, *args, **kws):
+        return self.raw_command(
+            'clone', capture_stdout=False, capture_stderr=False, *args, **kws)
+
+    def commit(self, *args, **kws):
+        return self.raw_command('commit', *args, **kws)
+
+    def config(self, *args, **kws):
+        return self.raw_command_with_output('config', *args, **kws)
+
+    def fetch(self, *args, **kws):
+        return self.raw_command(
+            'fetch', capture_stdout=False, capture_stderr=False, *args, **kws)
+
+    def log(self, *args, **kws):
+        return self.raw_command_with_output('log', *args, **kws)
+
+    def ls_remote(self, *args, **kws):
+        return self.raw_command_with_output('ls-remote', *args, **kws)
+
+    def push(self, *args, **kws):
+        return self.raw_command(
+            'push', capture_stdout=False, capture_stderr=False, *args, **kws)
+
+    def rev_list(self, *args, **kws):
+        return self.raw_command_with_output('rev-list', *args, **kws)
+
+    def rev_parse(self, *args, **kws):
+        return self.raw_command_with_output('rev-parse', *args, **kws)
+
+    def rm(self, *args, **kws):  # pylint: disable=C0103
+        return self.raw_command('rm', *args, **kws)
+
+    def tag(self, *args, **kws):
+        return self.raw_command_with_output('tag', *args, **kws)
