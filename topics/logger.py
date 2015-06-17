db6ae4dfcff637e6e9329145791757645d7a1dde
ccc <cadappl@gmail.com>
2015-06-17 23:39:31 +0800

    logging: use countable verbose to enable the log
    The commit changes the behaviour of "verbose" as repeated options
    to increase the logging level:
    
     -v:     logging.CRITICAL
     -vv:    logging.ERROR
     -vvv:   logging.INFO
     -vvvv:  logging.DEBUG
     -vvvvv: logging.NOTSET
    
    The environment KREP_VERBOSE will be detected if no verbose is
    set by default. It can be used to debug the function before the
    actual options are analyzed and set successfully.
    
    Change-Id: I9e969f5a085ebf21c3490734ccf38a818cc66f81

--- topics/logger.py
+++ topics/logger.py
@@ -1,19 +1,60 @@
 
 import logging
+import os
+import re
 import threading
 
 
+_level = -1  # pylint: disable=C0103
 _ldata = threading.local()  # pylint: disable=C0103
 
 
 class Logger(object):
     """Provides the logging methods"""
+
+    CRITICAL = logging.CRITICAL
+    ERROR = logging.ERROR
+    WARNING = logging.WARNING
+    INFO = logging.INFO
+    DEBUG = logging.DEBUG
+    NOTSET = logging.NOTSET
+
+    LEVEL_MAP = {
+        0: CRITICAL,
+        1: ERROR,
+        2: WARNING,
+        3: INFO,
+        4: DEBUG,
+        5: NOTSET
+    }
+
     @staticmethod
-    def set(level=None, newformat=None):
+    def set(verbose=None, level=None, name=None, newformat=None):
+        global _level  # pylint: disable=C0103,W0603
+        if verbose is None and _level < 0:
+            krep_verbose = os.environ.get('KREP_VERBOSE', '')
+            if re.match(r'-?\d+', krep_verbose):
+                verbose = int(krep_verbose)
+
+        if verbose is not None and verbose > -1:
+            level = Logger.LEVEL_MAP.get(verbose, Logger.DEBUG)
+
         if newformat:
             logging.basicConfig(format=newformat, level=level)
         else:
             logging.basicConfig(format='%(name)s: %(message)s', level=level)
+
+        if _level < 0 or (verbose > 0 and level < _level):
+            _level = level or logging.ERROR
+
+        if level is None:
+            level = _level
+
+        logger = Logger.get_logger(name or 'root')
+        if level is not None:
+            logger.setLevel(level)
+
+        return logger
 
     @staticmethod
     def get_logger(name=None):
@@ -26,6 +67,9 @@ class Logger(object):
         else:
             if hasattr(_ldata, 'name'):
                 name = _ldata.name
+
+        if _level < 0:
+            Logger.set()
 
         return logging.getLogger(name or 'root')
 
