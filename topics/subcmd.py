08de0e839909c5d17673e568bdd78cf4f348d2b7
ccc <cadappl@gmail.com>
2016-06-23 07:23:45 +0800

    subcmd: support multiple thread with option job
    It provides the class with multiple thread support to speed
    up the command execution in parallel.
    
    The option "job" would be shown automatically only with the multiple
    thread command is inherited by implemented sub-commands.
    
    Change-Id: I1c9d42e6f1dbb5e4686a850060ac70d3d249d14f

--- topics/subcmd.py
+++ topics/subcmd.py
@@ -1,4 +1,5 @@
 
+import threading
 from logger import Logger
 
 
@@ -26,7 +27,17 @@ class SubCommand(object):
 
     def options(self, optparse, *args, **kws):  # pylint: disable=W0613
         """Handles the options for the subcommand."""
-        pass
+        self._options_jobs(optparse)
+
+    def _options_jobs(self, optparse):
+        if self.support_jobs():
+            options = optparse.get_option_group('--force') or \
+                optparse.add_option_group('Other options')
+
+            options.add_option(
+                '-j', '--job',
+                dest='job', action='store', type='int',
+                help='jobs to run with specified threads in parallel')
 
     @staticmethod
     def get_logger():
@@ -42,16 +53,68 @@ class SubCommand(object):
         """Sets the subcommand name."""
         self._name = name
 
-    @staticmethod
-    def override_value(va, vb=False):
-        """Overrides the late values if it's a boolean one."""
-        def _is_bool(value):
-            return isinstance(value, bool)
+    def support_jobs(self):  # pylint: disable=W0613
+        """Indicates if the command can run with threading."""
+        return False
 
-        return ((_is_bool(va) and va) and not (_is_bool(vb) and not vb)) or va
+    @staticmethod
+    def override_value(va, vb=None):
+        """Overrides the late values if it's not a boolean value."""
+        return vb if vb is not None else va
 
     def execute(self, options, *args, **kws):  # pylint: disable=W0613
         return True
 
 
-TOPIC_ENTRY = 'SubCommand'
+class SubCommandWithThread(SubCommand):
+    """Commands with threading method to run with multiple jobs"""
+    def support_jobs(self):  # pylint: disable=W0613
+        return True
+
+    def run_with_thread(self, jobs, tasks, func, *args):
+        def _run(task, sem, event, func, args):
+            try:
+                if len(args) > 0:
+                    func(task, *args)
+                else:
+                    func(task)
+            except KeyboardInterrupt:
+                if event:
+                    event.set()
+            except Exception, e:  # pylint: disable=W0703
+                self.get_logger().exception(e)
+                event.set()
+            finally:
+                sem.release()
+
+        ret = True
+        if jobs > 1:
+            threads = set()
+            sem = threading.Semaphore(jobs)
+            event = threading.Event()
+
+            for task in tasks:
+                if event.isSet():
+                    break
+
+                sem.acquire()
+                thread = threading.Thread(
+                    target=_run,
+                    args=(task, sem, event, func, args))
+                threads.add(thread)
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+
+            if event.isSet():
+                print '\nerror: Exited due to errors.'
+                ret = False
+        else:
+            for task in tasks:
+                ret = func(task, *args) and ret
+
+        return ret
+
+
+TOPIC_ENTRY = 'SubCommand, SubCommandWithThread'
