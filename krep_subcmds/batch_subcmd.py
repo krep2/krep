0b72119acdd70622feef16aa0390eb380f900764
ccc <cadappl@gmail.com>
2016-09-17 20:30:16 +0800

    batch: adapt the change for option value equaling to the default
    It adaptes Ib96bf1b764ed7ee565f1a3d4c173d7e4222b4eb8 to adjust
    the usage of the updated methods.
    
    Change-Id: I84d36520a63dad2d487d55008ef2797f6ee098ed

--- krep_subcmds/batch_subcmd.py
+++ krep_subcmds/batch_subcmd.py
@@ -104,7 +104,7 @@ to define the projects in the config file.
                     proj_name = conf.get_subsection_name(name)
                     setattr(project, 'name', proj_name)
                     if _filter_with_group(project, proj_name, options.group):
-                        project.join(default)
+                        project.join(default, override=False)
                         projects.append(project)
 
             projs, nprojs = list(), list()
@@ -122,7 +122,7 @@ to define the projects in the config file.
                     setattr(
                         project, 'working_dir', os.path.abspath(working_dir))
 
-                project.join(options)
+                project.join(options, override=False)
                 if multiple:
                     projs.append(project)
                 else:
