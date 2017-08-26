90ba6dafe4aa077b4f9dfd746378b4695a36057b
ccc <cadappl@gmail.com>
2017-08-26 14:07:39 +0800

    file_utils: remove the file before copy
    the operation on file mode might be failed becasue of the soft-link,
    it'll be simple to just remove the file before copy.
    
    Change-Id: Ie22d79584657101a59284550518475d171d53e4e

--- topics/files/file_utils.py
+++ topics/files/file_utils.py
@@ -93,10 +93,10 @@ class FileUtils(object):
 
             os.symlink(linkto, dest)
         else:
-            if os.path.lexists(dest):
-                mode = os.lstat(dest)[stat.ST_MODE]
-                mode |= stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH
-                os.chmod(dest, mode)
+            if os.path.isdir(dest):
+                shutil.rmtree(dest)
+            elif os.path.lexists(dest):
+                os.unlink(dest)
 
             shutil.copy2(src, dest)
 
