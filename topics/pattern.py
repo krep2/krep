9c9101271ca90b6b3baed6d10da98164b8460c26
ccc <cadappl@gmail.com>
2018-02-09 23:14:04 +0800

    pattern: support "continue" override in xml
    the expression in element "value" contains the "continue"
    attribute implicitly. the change provides the overrided
    mechanism to the expression without element "replace".
    
    Change-Id: I22171e59dbf17735e9265c586a86e23edd4c275c

--- topics/pattern.py
+++ topics/pattern.py
@@ -28,7 +28,7 @@ class PatternItem(object):
 
     def __init__(self, category, patterns=None, exclude=False, name=None):
         self.name = name
-        self.cont = False
+        self.cont = True
         self.include = list()
         self.exclude = list()
         self.subst = list()
@@ -93,7 +93,7 @@ class PatternItem(object):
     def continuable(self):
         return self.cont
 
-    def split(self, patterns):
+    def split(self, patterns, cont=None):
         inc, exc, rep = list(), list(), list()
         patterns = patterns.strip()
 
@@ -105,6 +105,7 @@ class PatternItem(object):
                     rep.append(
                         PatternReplaceItem(
                             items[1] or self.name, items[2],
+                            cont if cont is not None else
                             pattern.startswith(
                                 PatternItem.CONN_REPLACE_DELIMITER)))
             elif pattern.startswith(PatternItem.OPPOSITE_DELIMITER):
@@ -114,8 +115,8 @@ class PatternItem(object):
 
         return inc, exc, rep
 
-    def add(self, patterns='', exclude=False, subst=None):
-        inc, exc, rep = self.split(patterns)
+    def add(self, patterns='', exclude=False, subst=None, cont=None):
+        inc, exc, rep = self.split(patterns, cont)
         if exclude:
             inc, exc = exc, inc
 
@@ -163,7 +164,8 @@ def _attr(node, attribute, default=None):
 
 
 class PatternFile(object):  # pylint: disable=R0903
-    _XmlPattern = namedtuple('_XmlPattern', 'category,name,value,replacement')
+    _XmlPattern = namedtuple(
+        '_XmlPattern', 'category,name,value,replacement,cont')
 
     @staticmethod
     def parse_pattern(node, patterns=None, exclude=False, replacement=False):
@@ -171,18 +173,33 @@ class PatternFile(object):  # pylint: disable=R0903
                 'pattern', 'exclude-pattern', 'rp-pattern', 'replace-pattern'):
             return None
 
+        def _ensure_bool(value):
+            if value is None:
+                return None
+            else:
+                value = value.lower()
+                if value in ('true', 'yes'):
+                    return True
+                elif value in ('false', 'no'):
+                    return False
+
+            return None
+
         p = PatternFile._XmlPattern(
             name=_attr(node, 'name', patterns and patterns.name),
             category=_attr(node, 'category', patterns and patterns.category),
-            value=_attr(node, 'value'),
+            value=_attr(node, 'value') or _attr(node, 'name'),
             replacement=_attr(node, 'replace')
-            if node.nodeName != 'exclude-pattern' or replacement else None)
+            if node.nodeName != 'exclude-pattern' or replacement else None,
+            cont=_ensure_bool(_attr(node, 'continue')))
 
-        if p.replacement is not None:
+        if p.replacement is not None or PatternItem.is_replace_str(p.value):
             pi = PatternItem(category=p.category, name=p.name)
-            pi.add(
-                subst=PatternReplaceItem(
-                    p.value, p.replacement, _attr(node, 'continue') != 'false'))
+            if p.replacement:
+                pi.add(
+                    subst=PatternReplaceItem(p.value, p.replacement, pi.cont))
+            else:
+                pi.add(p.value, cont=p.cont)
         else:
             pi = PatternItem(
                 category=p.category, patterns=p.value,
@@ -205,7 +222,7 @@ class PatternFile(object):  # pylint: disable=R0903
                 'replace-patterns'):
             parent = PatternFile._XmlPattern(
                 name=_attr(node, 'name'), category=_attr(node, 'category'),
-                value=None, replacement=None)
+                value=None, replacement=None, cont=True)
 
             for child in node.childNodes:
                 pi = PatternFile.parse_pattern(
