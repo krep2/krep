7b6275cc2ac1284aef5d067be27022bc905f1d3e
ccc <cadappl@gmail.com>
2018-02-20 19:52:25 +0800

    pki: create the tags with refs with option local
    it's purposed to create the right tags if the option 'local' used.
    and the import can be pushed manually with more investigation later.
    
    Change-Id: I1e689b5e20c614decddf836e40cfd6a69dff039e

--- krep_subcmds/pkg_import_subcmd.py
+++ krep_subcmds/pkg_import_subcmd.py
@@ -408,9 +408,19 @@ The escaped variants are supported for the imported files including:
                 if ret == 0:
                     if options.version_template:
                         tags.append(options.version_template % tmpl)
+                    elif options.local:
+                        trefs = self.override_value(  # pylint: disable=E1101
+                            options.refs, options.tag_refs) or ''
+                        if trefs:
+                            trefs += '/'
+
+                        tags.append(
+                            '%s%s%s' % (
+                                trefs, options.version_prefix, revision))
                     else:
                         tags.append(
                             '%s%s' % (options.version_prefix, revision))
+
                     ret, _ = project.tag(tags[-1])
 
             if os.path.lexists(temp):
