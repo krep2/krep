cc5687524e49a8e05274a787174d7c3cb7233690
ccc <cadappl@gmail.com>
2018-09-18 21:25:12 +0800

    repo_cmd: add the file for git-repo commands
    Change-Id: Ib9dae7b5a4efee8950d6493e10bc7b626437cd3e

---
+++ topics/repo_cmd.py
@@ -0,0 +1,25 @@
+
+from command import Command
+
+
+class RepoCommand(Command):
+    """Executes a repo sub-command with specified parameters"""
+    def __init__(self, *args, **kws):
+        Command.__init__(self, *args, **kws)
+        self.repo = FileUtils.find_execute('repo')
+
+    def _execute(self, *args, **kws):
+        cli = list()
+        cli.append(self.repo)
+
+        if len(args):
+            cli.extend(args)
+
+        self.new_args(cli, self.get_args())
+        return self.wait(**kws)
+
+    def init(self, *args, **kws):
+        return self._execute('init', *args, **kws)
+
+    def sync(self, *args, **kws):
+        return self._execute('sync', *args, **kws)
