02806620fd257eb46f3719fe216c0f95dd6c5aeb
ccc <cadappl@gmail.com>
2018-10-31 22:12:08 +0800

    pki: support git-push option "--date"
    Change-Id: Iebd6c688304eaefc118364880b0bb6fd8ba474c2

--- krep_subcmds/pkg_import_subcmd.py
+++ krep_subcmds/pkg_import_subcmd.py
@@ -346,7 +346,12 @@ The escaped variants are supported for the imported files including:
 
             args.append('-m')
             args.append(message)
-            args.append('--date="%s"' % time.ctime(timestamp))
+
+            optgc = options.extra_values(options.extra_option, 'git-commit')
+            if optgc and optgc.date:
+                args.append('--date="%s"' % optgc.date.strip('\'"'))
+            else:
+                args.append('--date="%s"' % time.ctime(timestamp))
 
             ret = project.commit(*args)
 
