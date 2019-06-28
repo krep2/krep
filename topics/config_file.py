a7cb2876f01c2948d2a15547aa2bc004c2f1a0e7
ccc <cadappl@gmail.com>
2019-06-28 19:54:42 +0800

    config_file: parse included xml with customized class
    Change-Id: Id54a57389a547c968866f89ebe5d83fbc0da2f57

--- topics/config_file.py
+++ topics/config_file.py
@@ -305,12 +305,16 @@ class XmlConfigFile(_ConfigFile):
 
         self.sets[name].append(attrs)
 
-    def parse_include(self, node):
+    def parse_include(self, node, clazz=None):
         name = self.get_attr(node, 'name')
         if name and not os.path.isabs(name):
             name = os.path.join(os.path.dirname(self.filename), name)
 
-        xvals = XmlConfigFile(name, self.get_default())
+        if issubclass(clazz, XmlConfigFile):
+            xvals = clazz(name, self.get_default())
+        else:
+            xvals = XmlConfigFile(name, self.get_default())
+
         # duplicate the 'value-sets'
         for key, value in xvals.sets.items():
             if key not in self.sets:
