773ea89dc250b42d73a77edc94bbd10826bb9b46
ccc <cadappl@gmail.com>
2019-08-31 20:37:39 +0800

    config_file: correct the match pattern
    Change-Id: I242da3de8ad8c6961144b55fdd734fd7dbb2b5df

--- topics/config_file.py
+++ topics/config_file.py
@@ -432,7 +432,7 @@ class XmlConfigFile(_ConfigFile):
                 return var.get(varname)
 
         i, nonexisted = 0, False
-        varprog = re.compile(ur'\$(\w+|\{[^}]*\}|\([^)]*\))')
+        varprog = re.compile(r'\$(\w+|\{[^}]*\}|\([^)]*\))')
         if var is None:
             var = {}
             var.update(self.vars)
