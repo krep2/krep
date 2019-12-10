cf0b7685edb05c8e56a8c636e2e74161ea5eb93f
ccc <cadappl@gmail.com>
2019-12-10 09:45:05 +0800

    subcmd: execute thread tasks in alphabet order
    Change-Id: Id86611b69c5141c07e13d9c46f56c3b25376220b

--- topics/subcmd.py
+++ topics/subcmd.py
@@ -359,7 +359,7 @@ class SubCommandWithThread(SubCommand):
             sem = threading.Semaphore(jobs)
             event = threading.Event()
 
-            for task in tasks:
+            for task in sorted(tasks):
                 if event.isSet():
                     break
 
@@ -377,7 +377,7 @@ class SubCommandWithThread(SubCommand):
                 self.get_logger().error('Exited due to errors')
                 ret = False
         else:
-            for task in tasks:
+            for task in sorted(tasks):
                 ret = func(task, *args) and ret
 
         return ret
